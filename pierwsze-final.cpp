5ef902f1e8c2b1946f9aea1dd73ff2c2202f3f89
WiktorNorek <wiktornorko17@gmail.com>
2022-03-31 09:31:29 +0200

    Added final version of pierwsze

---
+++ pierwsze-final.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n; // ilosc
+	int lp = 0; //pierwsze
+	int p = 2; // liczby zwykłe
+	bool x;
+
+	cout << "Ile liczb" << endl;
+	cin >> n;
+
+	while (lp < n) {
+		x = true;
+
+		for (int d = 2; d < p - 1; d++) {
+			if (p % d == 0) {
+				x = false;
+			}
+		}
+		if (x == true) {
+			cout << p << ", ";
+			lp = lp + 1;
+		}
+		p = p + 1;
+	}
+	return 0;
+}
