464e3dbed27ef177521793cbad9c69c42af7fdc4
WiktorNorek <wiktornorko17@gmail.com>
2022-04-28 09:32:30 +0200

    Komentarz

---
+++ cezara.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+void funk1(char tab[], int klucz2);
+
+int main()
+{
+    char tab[1000];
+    int klucz;
+    funk1(tab, klucz);
+}
+
+
+void funk1(char tab[], int klucz2)
+{
+    int dl;
+    dl = strlen(tab);
+    if (!(klucz2 >= -26 && klucz2 <= 26)) {
+        return;
+    }
+
+    if (klucz2 >= 0) {
+        for (int i; i < dl; i++) {
+            if (tab + klucz2 <= 'z') {
+                tab += klucz2;
+            }
+            else {
+                tab = tab + klucz2 - 26;
+            }
+        }
+    }
+    else {
+        for (int d = 0; d < dl; d++) {
+            if (tab[d] + klucz2 >= 'a') {
+                tab[d] += klucz2;
+            }
+            else {
+                tab[d] = tab[d] + klucz2 + 26;
+            }
+        }
+    }
+}
---
+++ cezara/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+void funk1(char tab[], int klucz2);
+
+int main()
+{
+    char tab[1000];
+    cout << "Podaj słowo: ";
+    cin >> tab;
+    cout << endl;
+    int klucz;
+    cout << "Podaj klucz: ";
+    cin >> klucz;
+    cout << endl;
+    funk1(tab, klucz);
+    cout << "Zaszyfrowane słowo: " << tab << endl;
+    funk1(tab, -klucz);
+    cout << "Odszyfrowane słowo: " << tab << endl;
+}
+
+
+void funk1(char tab[], int klucz2)
+{
+    int dl;
+    dl = strlen(tab);
+    if (!(klucz2 >= -26 && klucz2 <= 26)) {
+        return;
+    }
+
+    if (klucz2 >= 0) {
+        for (int i = 0; i < dl; i++) {
+            if (tab[i] + klucz2 <= 'z') {
+                tab[i] += klucz2;
+            }
+            else {
+                tab[i] = tab[i] + klucz2 - 26;
+            }
+        }
+    }
+    else {
+        for (int d = 0; d < dl; d++) {
+            if (tab[d] + klucz2 >= 'a') {
+                tab[d] += klucz2;
+            }
+            else {
+                tab[d] = tab[d] + klucz2 + 26;
+            }
+        }
+    }
+}
